15de9dceb72931848c9cd74fd106b20e6639608e
yudharief <yudharif58@gmail.com>
2022-10-28 14:05:01 +0700

    typescript day1

---
+++ day1.js
@@ -0,0 +1,7 @@
+function returntext(text, index1, index2) {
+    return text;
+}
+var text = "Learning Typescript is different than Javascript";
+var index1 = 9;
+var index2 = 19;
+console.log(returntext(text, index1, index2));
---
+++ day1.ts
@@ -0,0 +1,8 @@
+function returntext(text: string, index1: number, index2: number): string{
+    return text;
+}
+const text = "Learning Typescript is different than Javascript";
+const index1 = 9;
+const index2 = 19;
+
+console.log(returntext(text, index1, index2));
