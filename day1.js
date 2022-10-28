28116f10c1280d05c02a4b1c9ee252a5176c90f1
yudharief <yudharif58@gmail.com>
2022-10-28 14:05:31 +0700

    ts day1

--- day1.js
+++ day1.js
@@ -4,4 +4,4 @@ function returntext(text, index1, index2) {
 var text = "Learning Typescript is different than Javascript";
 var index1 = 9;
 var index2 = 19;
-console.log(returntext(text, index1, index2));
+console.log(returntext(text, index1, index2)); 
